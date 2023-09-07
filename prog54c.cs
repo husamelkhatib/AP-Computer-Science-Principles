9c60645673eb0b2fd4a5ca22b170acd186e5326b
husamelkhatib <112715228+husamelkhatib@users.noreply.github.com>
2023-09-07 14:12:01 +0000

    update

--- prog54c.cs
+++ prog54c.cs
@@ -9,7 +9,6 @@ using System;
     double rad = Convert.ToDouble(Console.ReadLine());
     double area = pi * rad * rad;
     double circ = 2 * pi * rad;
-
     Console.WriteLine("The radius is: " + rad);
     Console.WriteLine("The area is: " + area);
     Console.WriteLine("The circumference is: " + circ);
