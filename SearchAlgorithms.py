7933c39667da68c0f70a5c3ad02e50446c660f83
husamelkhatib <112715228+husamelkhatib@users.noreply.github.com>
2024-01-18 09:02:16 -0600

    Update SearchAlgorithms.py
    fsjhadkjhfd

--- SearchAlgorithms.py
+++ SearchAlgorithms.py
@@ -1,17 +1,17 @@
 def linear_search(lst, target):
-    for i in range(len(lst)):
-        if lst[i] == target:
-            return i
-    return -1
+  for i in range(len(lst)):
+      if lst[i] == target:
+          return i
+  return -1
 
 def binary_search(lst, target):
-    left, right = 0, len(lst) - 1
-    while left <= right:
-        mid = left + (right - left) // 2
-        if lst[mid] == target:
-            return mid
-        elif lst[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return -1
+  left, right = 0, len(lst) - 1
+  while left <= right:
+      mid = left + (right - left) // 2
+      if lst[mid] == target:
+          return mid
+      elif lst[mid] < target:
+          left = mid + 1
+      else:
+          right = mid - 1
+  return -1
