0a1e4617bc747ca0aa35a0d1db50199a1a51c7b7
husamelkhatib <112715228+husamelkhatib@users.noreply.github.com>
2024-01-18 09:01:26 -0600

    Update prog408b.py
    jfhjakfafdad

--- prog408b.py
+++ prog408b.py
@@ -1,8 +1,21 @@
 import time
-from SearchAlgorithms import linear_search, binary_search
-def print_search(type, result, time):
-if result != -1:
-    print(type + "Number found at index" + result)
-else:
-    print(type + "Number not found")
-print("Search time: " + time + "seconds")
+from SearchAlgorithms import *
+def main():
+  try:
+    data = []
+    with open("prog408b.dat", 'r') as f:
+      data = [int(line) for line in f]
+    for lcv in range(3):
+      num = int(input("enter a number: "))
+      startTime = time.perf_counter()
+      result = linear_search(data, num)
+      endTime = time.perf_counter()
+      print(result)
+      
+  except Exception as e:
+    print("Error:", e)
+  pass
+
+
+if __name__ == "__main__":
+  main()
